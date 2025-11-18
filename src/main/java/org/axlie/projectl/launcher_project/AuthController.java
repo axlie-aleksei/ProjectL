fe0f156e1123c40896ae62316f1af796d4efd7b1
Your Name <you@example.com>
2025-11-18 11:23:20 +0200

    created params for username and pass

--- src/main/java/org/axlie/projectl/launcher_project/AuthController.java
+++ src/main/java/org/axlie/projectl/launcher_project/AuthController.java
@@ -3,9 +3,12 @@ package org.axlie.projectl.launcher_project;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Array;
+
 @RestController
 @RequestMapping("/api")
 public class AuthController {
+
     private final AuthService authService;
     private final JwtService jwtService;
 
@@ -16,8 +19,16 @@ public class AuthController {
 
     @PostMapping("/registration")
     public String registration(@RequestParam String username, @RequestParam String password) {
-        return authService.registration(username, password);
-    }
+        if (password.length() <= 5) {
+            return "password cannot be less than 6 characters";
+        } else if (!username.matches("^[a-zA-Z0-9]+$")) {
+            return "You are cannot enter this symbols(!@#$%^&*()_+-<>?|,./[]=";
+        }else if (!password.matches("^[^\\p{IsCyrillic}\s]+$")){
+            return "ebanat?";
+        }else
+            return authService.registration(username, password);
+        }
+
 
     @PostMapping("/login")
     public String login(@RequestParam String username, @RequestParam String password) {
