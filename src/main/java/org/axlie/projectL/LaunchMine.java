46637540d7c3d655b834b413998dd166dfe58552
Terabyte23 <aleksandr.ag050507@gmail.com>
2025-11-27 15:07:29 +0200

    design button and show password icon (NOT FINAL)2.0

--- src/main/java/org/axlie/projectL/LaunchMine.java
+++ src/main/java/org/axlie/projectL/LaunchMine.java
@@ -2,15 +2,12 @@ package org.axlie.projectL;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 import java.io.*;
-import java.net.URL;
-import java.net.URLConnection;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
+import java.net.*;
+import java.nio.file.*;
 import java.util.List;
+import java.util.Objects;
 
 public class LaunchMine extends JFrame {
 
@@ -18,24 +15,58 @@ public class LaunchMine extends JFrame {
     private final JProgressBar progBar;
 
     public LaunchMine() {
-
         setTitle("Axlie Project L");
-        setSize(400, 150);
+        setSize(600, 400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
-        setLayout(new BorderLayout(10, 10));
+        setResizable(true);
 
-        actButton = new JButton("download");
+        JPanel mainPanel = new JPanel() {
+            Image bg = new ImageIcon(getClass().getResource("/minecraft.png")).getImage();
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                g.drawImage(bg, 0, 0, getWidth(), getHeight(), this);
+            }
+        };
+        mainPanel.setLayout(new BorderLayout());
+        setContentPane(mainPanel);
+
+        JPanel formPanel = new JPanel(new GridBagLayout()) {
+            @Override
+            protected void paintComponent(Graphics g) {
+                Graphics2D g2 = (Graphics2D) g.create();
+                g2.setColor(new Color(30, 30, 30, 180));
+                g2.fillRoundRect(0, 0, getWidth(), getHeight(), 15, 15);
+                g2.dispose();
+                super.paintComponent(g);
+            }
+        };
+        formPanel.setOpaque(false);
+        formPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+
+        JPanel bottomPanel = new JPanel();
+        bottomPanel.setLayout(new BoxLayout(bottomPanel, BoxLayout.Y_AXIS));
+        bottomPanel.setOpaque(false);
+
         progBar = new JProgressBar(0, 100);
         progBar.setStringPainted(true);
-        progBar.setVisible(true);
+        progBar.setVisible(false);
+        progBar.setPreferredSize(new Dimension(300, 20));
+        bottomPanel.add(progBar);
 
-        JPanel center = new JPanel(new BorderLayout());
-        center.add(progBar, BorderLayout.WEST);
-        center.add(actButton);
-        add(center, BorderLayout.EAST);
-
+        actButton = new JButton(new ImageIcon(Objects.requireNonNull(getClass().getResource("/DOWNLOAD.png"))));
+        actButton.setPreferredSize(new Dimension(200, 40));
+        actButton.setFocusPainted(false);
+        actButton.setContentAreaFilled(false);
+        actButton.setBorder(null);
         actButton.addActionListener(e -> buttonDo());
+        bottomPanel.add(Box.createVerticalStrut(10));
+        bottomPanel.add(actButton);
+
+        formPanel.add(bottomPanel);
+
+        mainPanel.add(formPanel, BorderLayout.CENTER);
     }
 
     public void buttonDo() {
@@ -43,27 +74,25 @@ public class LaunchMine extends JFrame {
         progBar.setVisible(true);
 
         SwingWorker<Void, Integer> worker = new SwingWorker<>() {
-
-
             @Override
             protected Void doInBackground() throws Exception {
-
                 URL url = new URL("http://localhost:8080/docs/download/4");
                 InputStream in = url.openStream();
                 URLConnection conn = url.openConnection();
 
                 String disposition = conn.getHeaderField("Content-Disposition");
                 String fileName = "downloaded_file.bin";
-                if (disposition != null ) {
-                    fileName = disposition.split("filename=")[1].replace("\"","").trim();
+                if (disposition != null) {
+                    fileName = disposition.split("filename=")[1].replace("\"", "").trim();
                 }
+
                 Path outputDir = Paths.get("C:\\downloads");
                 Files.createDirectories(outputDir);
 
                 Path output = outputDir.resolve(fileName);
 
                 try (BufferedInputStream bis = new BufferedInputStream(in);
-                OutputStream out = new FileOutputStream(output.toFile())) {
+                     OutputStream out = new FileOutputStream(output.toFile())) {
 
                     byte[] buffer = new byte[4096];
                     int read;
@@ -74,14 +103,11 @@ public class LaunchMine extends JFrame {
                         out.write(buffer, 0, read);
                         total += read;
 
-                        if (length > 0){
+                        if (length > 0) {
                             int percent = (int) ((total * 100) / length);
                             publish(percent);
-//                        }else {
-//                            publish((int) (total % 100));
                         }
                     }
-
                 }
 
                 return null;
@@ -98,16 +124,15 @@ public class LaunchMine extends JFrame {
                 actButton.setText("Launch");
                 actButton.setEnabled(true);
             }
-
         };
+
         worker.execute();
     }
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            new LaunchMine().setVisible(true);
-        });
+        SwingUtilities.invokeLater(() -> new LaunchMine().setVisible(true));
     }
-
 }
+
+
 
