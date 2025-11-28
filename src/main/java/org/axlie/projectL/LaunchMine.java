4a1a36e12bc14395821cadde4eead5110a898010
axlie <aleksei.dubtsak@ivkhk.ee>
2025-11-28 10:49:32 +0200

    done functions in LaunchMine frame (minecraft download, lunch ) p.s: Create a folder selector, adapt the frame to the selected folder, and set the Minecraft username from code.

--- src/main/java/org/axlie/projectL/LaunchMine.java
+++ src/main/java/org/axlie/projectL/LaunchMine.java
@@ -2,8 +2,6 @@ package org.axlie.projectL;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.*;
 import java.net.URL;
 import java.net.URLConnection;
